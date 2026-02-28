81c9a8f62b7a5c002e0d38c58d5507594fe53b8d
Revewill <264042193+Revewill@users.noreply.github.com>
2026-02-28 17:55:54 +0800

    Update

--- W3/BLAST/T2/BLAST_T2.sh
+++ W3/BLAST/T2/BLAST_T2.sh
@@ -1,1 +1,8 @@
 #!/bin/bash
+
+# Function statement
+echo -e "This is a bash script that generates random sequences and align them based on BLAST algorithm."
+
+# Locate file and acquire permission
+read -t 60 -p "Type your file's directory here: ~/blast/" dirpath
+fulldirpath="/home/test/blast/${dirpath}"
